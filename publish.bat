e9e51f6b71e63b331d64e2c62bc5994b5b554182
Elias Sebastian Selenius Haneborg <ESSH@LIVE.NO>
2024-05-26 19:33:38 +0200

    update gitignore, publish script

---
+++ publish.bat
@@ -0,0 +1,12 @@
+
+@rmdir /q /s release
+@mkdir release\bin
+@copy output.exe release\bin
+@copy glfw3.dll release\bin
+@robocopy ..\grax\shaders release\grax\shaders /mir
+@copy ..\grax\CascadiaMono.bmp release\grax
+
+@mkdir release\bin\src
+@copy src\text_editor.pog release\bin\src
+
+butler push release EliasSelenius/textus-quill:win-x64
